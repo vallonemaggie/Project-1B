c4b5a7bd5e9cada69bd84e4c1afe4c0b327d8c55
vallonemaggie <vallonemaggie@gmail.com>
2018-02-27 10:40:58 -0600

    changed js/scss/html
    added interactive button

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
+var visibleText = "Show Less";
+var hiddenText = "Show More";
+
 $(document).ready(function () {
-    alert("jQuery ready");
+    //alert("jQuery ready");
+    
+    $(".info-box button").click(function(){
+        $(this).toggleClass("visible");
+        
+        if($(this).hasClass("visible") ){
+            $(this).html(visibleText);
+        }else{
+            $(this).html(hiddenText);
+        }
+        
+        $(this).next().toggleClass("visible");
+    });
 });
 
